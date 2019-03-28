423d9823a4603bca974a523bc1d76ca5ea6e27c9
rabbit860321 <rabbit860321@gmail.com>
2019-03-28 18:39:47 +0800

    first commit

---
+++ app/src/main/java/com/example/nkust_se/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.nkust_se;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                final Intent mainIntent = new Intent(MainActivity.this,Setting_Account.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, 5000);
+    }
+}
---
+++ app/src/main/java/com/example/nkust_se/Setting_Account.java
@@ -0,0 +1,13 @@
+package com.example.nkust_se;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Setting_Account extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting__account);
+    }
+}
