ce482f33b97569362785a6e2ab0423560d092135
rabbit860321 <rabbit860321@gmail.com>
2019-05-01 23:43:13 +0800

    setting screen name

---
+++ app/src/main/java/com/nkust_csie/se_project/Screen_History.java
@@ -0,0 +1,13 @@
+package com.nkust_csie.se_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Screen_History extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen_history);
+    }
+}
