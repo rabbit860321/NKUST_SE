252166b173aad022c367145b2a266c59fb19003f
rabbit860321 <rabbit860321@gmail.com>
2019-05-07 21:32:25 +0800

    logo 5sec jump to setting setting layout add scroll

--- app/src/main/java/com/nkust_csie/se_project/Screen_logo.java
+++ app/src/main/java/com/nkust_csie/se_project/Screen_logo.java
@@ -1,5 +1,7 @@
 package com.nkust_csie.se_project;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,5 +14,14 @@ public class Screen_logo extends AppCompatActivity {
 
         // 隱藏title Bar
         getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {  //5秒後跳轉到setting畫面
+            @Override
+            public void run() {
+                final Intent mainIntent = new Intent(Screen_logo.this,Screen_Setting.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, 5000);
     }
 }
