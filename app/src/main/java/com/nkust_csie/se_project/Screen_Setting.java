d7b0abf8973a139864e33710a4cdda8dfd9d0c0c
rabbit860321 <rabbit860321@gmail.com>
2019-06-07 18:17:54 +0800

    設定帳戶頁面 加入判斷帳戶名稱重複與金額為空功能

--- app/src/main/java/com/nkust_csie/se_project/Screen_Setting.java
+++ app/src/main/java/com/nkust_csie/se_project/Screen_Setting.java
@@ -56,21 +56,14 @@ public class Screen_Setting extends AppCompatActivity {
         settingvalue = new ContentValues();
         btn_account = (Button)findViewById(R.id.btn_account);
         btn_save = (Button)findViewById(R.id.btn_save);
-
         itlist_account = (ListView)findViewById(R.id.itlist_account);
 
-
-        settingCursor = db.query("tb_setting",null,null,null,null,null,null);  //查詢tb_setting所有資料
-        Log.e("TAG","tb_setting裡有"+settingCursor.getCount()+"筆資料");
-
-        if(settingCursor.getCount() != 0){
-            show_account_list();
-        }
+        show_account_list();
 
         btn_account.setOnClickListener(new View.OnClickListener() {  //新增帳戶
             @Override
             public void onClick(View v) {      //新增帳戶
-                showdialog(R.layout.dialog_account,1);
+                showdialog(R.layout.dialog_account);
             }
         });
 
@@ -78,12 +71,12 @@ public class Screen_Setting extends AppCompatActivity {
         btn_save.setOnClickListener(new View.OnClickListener() {   //儲存鍵
             @Override
             public void onClick(View v) {
-                settingCursor = db.query("tb_setting",null,null,null,null,null,null);
-                if(settingCursor.getCount() == 0){
-                    Toast toast = Toast.makeText(Screen_Setting.this,
-                            "請新增至少一筆帳戶資料!", Toast.LENGTH_LONG);
-                    //顯示Toast
-                    toast.show();
+                settingCursor = db.query("tb_setting",null,null,null,null,null,null);  //查詢tb_setting所有資料
+
+                if(settingCursor.getCount() == 0){  //須有一筆帳戶資料以上才能進到主畫面
+
+                    show_toast("請新增至少一筆帳戶資料");
+
                 }else{
                     final Intent mainIntent = new Intent(Screen_Setting.this,Screen_Main1.class);
                     startActivity(mainIntent);
@@ -94,7 +87,7 @@ public class Screen_Setting extends AppCompatActivity {
 
         itlist_account.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {  //點下帳戶列表的某一個item 可進行修改及刪除
 
                 final TextView account_list_id = (TextView) view.findViewById(R.id.account_list_id);            //list item 裡text的資料
                 final TextView account_list_name = (TextView) view.findViewById(R.id.account_list_name);
@@ -110,13 +103,20 @@ public class Screen_Setting extends AppCompatActivity {
                 final EditText account_money = EntryView.findViewById(R.id.account_money);
 
                 account_name.setText(account_list_name.getText().toString());
+                account_name.setFocusable(false);  //將名稱的EditText設為不可編輯
                 account_money.setText(account_list_money.getText().toString());
 
                 dialog.setPositiveButton("確認", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        DH.updateData(Integer.parseInt(account_list_id.getText().toString()),account_name.getText().toString(),account_money.getText().toString());
-                        show_account_list();
+
+                        //需判斷金額是否為空
+                        if(account_money.getText().toString().isEmpty()){
+                            show_toast("請輸入金額!");
+                        }else{
+                            DH.updateData(Integer.parseInt(account_list_id.getText().toString()),account_name.getText().toString(),account_money.getText().toString());
+                            show_account_list();
+                        }
                     }
                 });
                 dialog.setNegativeButton("刪除", new DialogInterface.OnClickListener() {
@@ -132,7 +132,7 @@ public class Screen_Setting extends AppCompatActivity {
         });
     }
 
-    private void showdialog(int layout,final int x){  //x判斷是哪個按鈕按下
+    private void showdialog(int layout){
         LayoutInflater inflater = LayoutInflater.from(Screen_Setting.this);
         final View EntryView = inflater.inflate(layout,null);
 
@@ -146,17 +146,23 @@ public class Screen_Setting extends AppCompatActivity {
         dialog.setPositiveButton("確認", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
+                //需判斷帳戶名稱是否重複 & 金額是否為空
+                ArrayList<String> ac_name = new ArrayList<String>();
+                settingCursor = db.query("tb_setting",null,null,null,null,null,null);
+                settingCursor.moveToFirst();
 
-                settingvalue.clear();
+                for(int i = 0;i<settingCursor.getCount();i++){              //把所有帳戶名稱存入陣列
+                    ac_name.add(settingCursor.getString(3));
+                    settingCursor.moveToNext();
+                }
 
-                switch (x) {
-                    case 1:  //設定帳戶
-                        DH.insertData(account_name.getText().toString(),account_money.getText().toString());
-                        /*settingvalue.put("Account",account_name.getText().toString());
-                        settingvalue.put("Money",Float.parseFloat(account_money.getText().toString()));
-                        db.insert("tb_setting", null, settingvalue);*/
-                        show_account_list();
-                        break;
+                if(ac_name.indexOf(account_name.getText().toString()) != -1){
+                    show_toast("帳戶名稱重複!");
+                }else if(account_money.getText().toString().isEmpty()){
+                    show_toast("請輸入金額!");
+                }else{
+                    DH.insertData(account_name.getText().toString(),account_money.getText().toString());
+                    show_account_list();
                 }
             }
         });
@@ -183,4 +189,12 @@ public class Screen_Setting extends AppCompatActivity {
         SimpleAdapter SA = new SimpleAdapter(this,items,R.layout.account_list_layout,new String[]{"_id","Account","Money"},new int[]{R.id.account_list_id,R.id.account_list_name,R.id.account_list_money});
         itlist_account.setAdapter(SA);
     }
+
+    private void show_toast(String text){
+        Toast toast = Toast.makeText(Screen_Setting.this,
+                text, Toast.LENGTH_LONG);
+        //顯示Toast
+        toast.show();
+    }
+
 }
