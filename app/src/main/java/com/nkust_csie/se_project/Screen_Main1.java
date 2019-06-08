ecd3798e2d98290ab7d851c330ab8429f08b8a6a
rabbit860321 <rabbit860321@gmail.com>
2019-06-08 22:20:50 +0800

    轉帳功能OK

--- app/src/main/java/com/nkust_csie/se_project/Screen_Main1.java
+++ app/src/main/java/com/nkust_csie/se_project/Screen_Main1.java
@@ -56,6 +56,8 @@ public class Screen_Main1 extends AppCompatActivity
     ContentValues cv;
     Calendar today;
 
+    Cursor tr_cs1,tr_cs2; //轉帳畫面用
+
     float today_cost_total;
 
     @Override
@@ -191,10 +193,12 @@ public class Screen_Main1 extends AppCompatActivity
                             final Intent gotoselclass = new Intent(Screen_Main1.this,Screen_Select_Class.class);
                             startActivity(gotoselclass);
                             break;
-                        case 1:
+                        case 1:  //收入
                             LayoutInflater inflater = LayoutInflater.from(Screen_Main1.this);
                             final View EntryView = inflater.inflate(R.layout.dialog_income,null);
+
                             final EditText edit_income_money = EntryView.findViewById(R.id.edit_income_money);
+                            final Spinner sp_income_account = EntryView.findViewById(R.id.sp_income_account);
 
                             AlertDialog.Builder dialog = new AlertDialog.Builder(Screen_Main1.this);
                             dialog.setView(EntryView);  //dialog設置自定義畫面
@@ -204,7 +208,6 @@ public class Screen_Main1 extends AppCompatActivity
                             final SimpleCursorAdapter adapter = new SimpleCursorAdapter(Screen_Main1.this, android.R.layout.simple_spinner_item ,cs, new String[] { "Account" }, new int[] {android.R.id.text1});
                             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-                            final Spinner sp_income_account = EntryView.findViewById(R.id.sp_income_account);
                             sp_income_account.setAdapter(adapter);
 
                             dialog.setPositiveButton("確定", new DialogInterface.OnClickListener() {
@@ -226,8 +229,84 @@ public class Screen_Main1 extends AppCompatActivity
                                     }
                                 }
                             });
+                            dialog.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                            });
 
                             dialog.show();
+                            break;
+                        case 2: //轉帳
+                            //須將左邊帳戶原金額扣掉使用者輸入的金額 並把右邊帳戶加上使用者輸入的金額
+                            LayoutInflater LI = LayoutInflater.from(Screen_Main1.this);
+                            final View EV = LI.inflate(R.layout.dialog_transfer,null);
+
+                            final Spinner sp_tr_1 = EV.findViewById(R.id.sp_tr_1);
+                            final Spinner sp_tr_2 = EV.findViewById(R.id.sp_tr_2);
+                            final EditText edit_tr_money = EV.findViewById(R.id.edit_tr_money);
+
+                            AlertDialog.Builder tr_dialog = new AlertDialog.Builder(Screen_Main1.this);
+                            tr_dialog.setView(EV);
+
+                            tr_cs1 = db.query("tb_setting",null,null,null,null,null,null);
+                            tr_cs2 = db.query("tb_setting",null,null,null,null,null,null);
+
+                            final SimpleCursorAdapter ad1 = new SimpleCursorAdapter(Screen_Main1.this, android.R.layout.simple_spinner_item ,tr_cs1, new String[] { "Account" }, new int[] {android.R.id.text1});
+                            ad1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                            final SimpleCursorAdapter ad2 = new SimpleCursorAdapter(Screen_Main1.this, android.R.layout.simple_spinner_item ,tr_cs2, new String[] { "Account" }, new int[] {android.R.id.text1});
+                            ad2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                            sp_tr_1.setAdapter(ad1);
+                            sp_tr_2.setAdapter(ad2);
+
+                            tr_dialog.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    //tr_cs1.getString(3)左邊帳戶名稱
+                                    //tr_cs1.getString(4)左邊帳戶金額
+                                    //tr_cs2.getString(3)右邊帳戶名稱
+                                    //tr_cs2.getString(4)右邊帳戶金額
+
+                                    /*Log.e("TAG",tr_cs1.getString(3)+tr_cs2.getString(3));
+                                    Log.e("TAG",tr_cs1.getString(4)+tr_cs2.getString(4));*/
+
+                                    if(edit_tr_money.getText().toString().isEmpty()){
+                                        show_toast("你沒輸入金額啦!");
+                                    }else if(tr_cs1.getString(3).equals(tr_cs2.getString(3))){
+                                        show_toast("請選擇兩個不一樣的帳戶");
+                                    }else if(Float.parseFloat(tr_cs1.getString(4)) < Float.parseFloat(edit_tr_money.getText().toString())){
+                                        show_toast("該帳戶餘額不足");
+                                    }else{
+                                        float money = Float.parseFloat(tr_cs1.getString(4)) - Float.parseFloat(edit_tr_money.getText().toString());
+                                        money = (float) (Math.round(money*100)/100.0);  //解決精度問題
+                                        ContentValues cv = new ContentValues();
+                                        cv.put("Money",money);
+                                        db.update("tb_setting",cv,"Account"+"='"+tr_cs1.getString(3)+"'",null);
+
+                                        cv.clear();
+
+                                        money = Float.parseFloat(tr_cs2.getString(4)) + Float.parseFloat(edit_tr_money.getText().toString());
+                                        money = (float) (Math.round(money*100)/100.0);  //解決精度問題
+                                        cv.put("Money",money);
+                                        db.update("tb_setting",cv,"Account"+"='"+tr_cs2.getString(3)+"'",null);
+
+                                        show_list_account();
+                                    }
+
+
+                                }
+                            });
+
+                            tr_dialog.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                            });
+
+                            tr_dialog.show();
+                            break;
                     }
                 }
             });
